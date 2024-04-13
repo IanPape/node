fc47d6810fb83cd0bb298561c8957527fdb9a9dc
Ian Pape <ianjamespape@gmail.com>
2024-04-13 10:28:41 -0400

    first commit

---
+++ step1.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+function cat(path) {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading ${path}:\n  ${err}`);
+      return;
+    }
+    console.log(data);
+  });
+}
+
+const filePath = process.argv[2];
+if (!filePath) {
+  console.error('Please provide a file path as an argument.');
+} else {
+  cat(filePath);
+}
